6d7d6960ba41806eb5d135aee0709f656c0132fb
DouglasSoares1203 <douglashardware13@gmail.com>
2022-11-30 10:11:21 -0300

    commit inicial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Route,Switch
+} from "react-router-dom";
+import Home from './pages/home';
+import Shop from './pages/shop';
+import './global.scss';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/components/directory/index.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import MenuItem from "../menu-item/index";
+import "./styles.scss";
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: [
+                {
+                    title: "bonés",
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1542060748-10c28b62716f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                    id: 1,
+                    linkUrl: "hats",
+                },
+                {
+                    title: "calças",
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1489987707025-afc232f7ea0f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                    id: 2,
+                    linkUrl: "jeans",
+                },
+                {
+                    title: "sapatos",
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                    id: 3,
+                    linkUrl: "shoes",
+                },
+                {
+                    title: "moletons",
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1537274942065-eda9d00a6293?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80",
+                    size: "large",
+                    id: 4,
+                    linkUrl: "hoodies",
+                },
+                {
+                    title: "bermudas",
+                    imageUrl:
+                        "https://images.unsplash.com/photo-1517423568366-8b83523034fd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80",
+                    size: "large",
+                    id: 5,
+                    linkUrl: "shorts",
+                },
+            ],
+        };
+    }
+
+    render() {
+        return (
+            <div className="directory-menu">
+                {this.state.sections.map(({ id, ...otherSectionProps }) => (
+                    <MenuItem key={id} {...otherSectionProps} />
+                ))}
+            </div>
+        );
+    }
+}
+
+export default Directory;
